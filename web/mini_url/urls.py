034ef19f39312bc10b7182606d830cba11788308
elwan <elwan7@gmail.com>
2016-03-10 22:24:45 +0000

    tutoriel django

--- web/mini_url/urls.py
+++ web/mini_url/urls.py
@@ -2,9 +2,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^add$',views.nouveau,name="url_nouveau"),
+    #url(r'^add$',views.nouveau,name="url_nouveau"),
+    url(r'^add$',views.UrlCreate.as_view(),name="url_nouveau"), #nouveau url basé sur la classe createview dans les vues génériques
+    #url(r'^edition/(?P<pk>\d)$',views.UrlUpdate.as_view(),name="url_update"),#Url pour éditer les raccourcis url
+    #faire un url avec code url comme identifiant pour modifier notre object
+    url(r'^edition/(?P<code>\w{6})$',views.UrlUpdate.as_view(),name='url_update'),
     url(r'^liste$',views.lire,name="url_liste"),
     url(r'^(?P<code>\w{6})/$',views.redirection,name="url_redirection"),
+    #url pour supprimer  une entree avec la class deleteview
+    url(r'^supp/(?P<code>\w{6})$',views.UrlDelete.as_view(),name="url_delete")
     
 ]
+
 
--- web/mini_url/views.py
+++ web/mini_url/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponseRedirect
 from mini_url.forms import MiniUrlForm
-from mini_url.models import MiniUrl 
+from mini_url.models import MiniUrl
+from django.views.generic import CreateView,UpdateView,DeleteView  
+from django.core.urlresolvers import reverse_lazy
+from django.contrib  import messages
 # Create your views here.
 
+#cette fonction peut etre Remplacer par createview 
 def nouveau(request):
     if request.method == 'POST':
         form = MiniUrlForm(request.POST)
@@ -26,3 +30,42 @@ def redirection(request,code):
     mini.save()
     
     return redirect(mini.url,permanent=True)
+
+#creer une vue de form avec Createview
+
+class UrlCreate(CreateView):
+    model = MiniUrl
+    template_name="mini_url/urlform.html"
+    form_class = MiniUrlForm
+    success_url = reverse_lazy(lire)
+    
+#Mise à jour des données avec updateview
+
+class UrlUpdate(UpdateView):
+    model= MiniUrl
+    template_name="mini_url/urlform.html"
+    form_class = MiniUrlForm
+    success_url = reverse_lazy(lire)
+
+    def get_object(self,queryset=None): #Recuperer les objet avec le code et nons avec la clé primaire en surchageant la methide get_object 
+        code = self.kwargs.get('code',None)
+        return get_object_or_404(MiniUrl,code=code)
+
+    def form_valid(self,form):
+        self.object= form.save()
+        #Envoyons un message à l'utilisateur
+        messages.success(self.request,"Vos informations ont été mise à jour.")
+        
+        return HttpResponseRedirect(self.get_success_url())
+    
+#supprimer un object avec les views 
+class UrlDelete(DeleteView):
+    model=MiniUrl
+    context_object_name="mini_url"
+    template_name="mini_url/supprimer.html"
+    success_url=reverse_lazy(lire)
+    form_class= MiniUrlForm
+
+    def get_object(self,queryset=None):
+        code =self.kwargs.get('code',None)
+        return get_object_or_404(MiniUrl,code=code)
