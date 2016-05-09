834885fe91317ff604ae452508f0bbee07495e9d
elwan <elwan7@gmail.com>
2016-05-09 13:49:01 +0000

    msi project

--- sms_appli/msi_sva/urls.py
+++ sms_appli/msi_sva/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns ,url
 from . import views
 
 urlpatterns =[
-    url(r'^test/(?P<msg>[\w\s]+)/',views.test),
-    url(r'^sms_save/(?P<id_message>\d{6})/(?P<numero_court>\d{5})/(?P<numero_telephone>\d{12})/(?P<msg>[\w\s]+)/(?P<mot_cle>\w+)/(?P<date_reception>\d{2}\-\d{2}\-\d{4})/$',views.save_sms),
+    url(r'^test/(?P<msg>"[\w\s]+")/',views.test),#test de capture des chaines de caractéres avec le regex 
+    url(r'^sms_save/(?P<id_message>\d{6})/(?P<numero_court>\d{5})/(?P<numero_telephone>\d{12})/(?P<msg>"[\w\s]+")/(?P<mot_cle>\w+)/(?P<date_reception>\d{4}\-\d{2}\-\d{2})/$',views.save_sms),
 ]
