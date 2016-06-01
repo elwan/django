0f0645a4bc22f02aa93e26a201a2b85852bc9a12
elwan <elwan7@gmail.com>
2016-06-01 16:07:25 +0000

    django project

--- textma/sva/urls.py
+++ textma/sva/urls.py
@@ -3,12 +3,12 @@ from . import views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns= [
-    url(r'^ajouter/',views.MessageCreate.as_view(),name="nouveau_message"),
-    url(r'^liste/',views.ListeMessage.as_view(),name="lister_message"),
+    #url(r'^ajouter/',views.MessageCreate.as_view(),name="nouveau_message"),
+    #url(r'^liste/',views.ListeMessage.as_view(),name="lister_message"),
     url(r'^msgenvoyer/',views.ListeMessageEnvoyes.as_view(),name="lister_message_envoyer"),
-    url(r'^editer/(?P<code>\w{6})$',views.MessageUpdate.as_view(),name="update_message"),
-    url(r'^delete/(?P<code>\w{6})$',views.MessageDelete.as_view(),name="delete_message"),
-    url(r'^envoi/(?P<code>\w{6})$',views.envoi_message,name="envoyer_message"),
+    #url(r'^editer/(?P<code>\w{6})$',views.MessageUpdate.as_view(),name="update_message"),
+    #url(r'^delete/(?P<code>\w{6})$',views.MessageDelete.as_view(),name="delete_message"),
+    #url(r'^envoi/(?P<code>\w{6})$',views.envoi_message,name="envoyer_message"),
     ##zone messages multiple
     url(r'^ajouter_multi/',views.MessageMultiCreate.as_view(),name="nouveau_message_multi"),
     url(r'editer_multi/(?P<code>\w{7})$',views.MessageMultiUpdate.as_view(),name="update_message_multi"),
