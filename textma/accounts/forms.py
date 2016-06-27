5e6c0deeab2f7f5880bf16aca6dc5df85e3e340f
elwan <elwan7@gmail.com>
2016-06-27 23:34:32 +0000

    django project

--- textma/accounts/forms.py
+++ textma/accounts/forms.py
@@ -9,22 +9,20 @@ class LoginForm(AuthenticationForm):
         password = forms.CharField(label="Password", max_length=30,widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password','type':'password','placeholder':'Password', 'required':'True'}))
 
 
-
-
-        
 class UserCreation(UserCreationForm):
         username = forms.CharField(label="Login", max_length=30,widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'login','placeholder':'Login','required':'True','autofocus':'True'}))
         first_name = forms.CharField(label="Prenom", max_length=30,widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'Prenom','placeholder':'Prénom','required':'True','autofocus':'True'}))
         last_name  = forms.CharField(label="Nom", max_length=30,widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'Nom','placeholder':'Nom','required':'True','autofocus':'True'}))
         numero_telephone= forms.CharField(label="Numero Téléphone", max_length=9,widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'Téléphone','placeholder':'Numero de téléphone','required':'True','autofocus':'True'}))
-        #date_naissance = forms.CharField(label="Date Naissance ",widget=forms.DateInput(attrs={'class':'datepicker', 'name': 'date_naissance','required':'True'}))
+        #date_naissance = forms.CharField(label="Date Naissance ",widget=forms.DateInput(attrs={'class':'date', 'name': 'date_naissance','required':'True'}))
         #date_naissance = forms.DateField(widget= AdminDateWidget())
                              
         email = forms.CharField(label="Email", max_length=30,widget=forms.EmailInput(attrs={'class': 'form-control', 'name': 'Email','placeholder':'Email','required':'True','autofocus':'True'}))
         password1 = forms.CharField(label="Password", max_length=30,widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password','placeholder':'password','required':'True','autofocus':'True'}))
         password2= forms.CharField(label="Password Validation", max_length=30,widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password_validation','placeholder':'password validation','required':'True','autofocus':'True'}))
         
-        #class Meta(UserCreationForm.Meta):
-        #        model = Profile
-        #        fields= UserCreationForm.Meta.fields
+        class Meta:
+                model = Profile
+                fields = ("username", "first_name", "last_name","numero_telephone","email", "password1", "password2")
+                
 
